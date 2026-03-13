29ab34de0e43e29f440c33fcaf885a3d567fcc2f
Fentanest <better0101@naver.com>
2026-03-13 14:47:48 +0900

    failed.sh 문구 추가

--- failed.sh
+++ failed.sh
@@ -1,1 +1,9 @@
+#!/bin/bash
+# Remove temporary failure states from coupon logs so they can be retried
 sed -i '/Failed/d' coupon_logs/*.txt
+sed -i '/Frequent/d' coupon_logs/*.txt
+sed -i '/frequent/d' coupon_logs/*.txt
+sed -i '/빈번한/d' coupon_logs/*.txt
+sed -i '/횟수가 초과/d' coupon_logs/*.txt
+sed -i '/Limit\/Rate/d' coupon_logs/*.txt
+sed -i '/Paused/d' coupon_logs/*.txt
